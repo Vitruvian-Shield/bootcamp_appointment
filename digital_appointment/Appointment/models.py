04991402e730c18f56f1950a38b818395aa55abc
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-08-05 01:01:08 +0330

    correct address

--- digital_appointment/Appointment/models.py
+++ digital_appointment/Appointment/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    provider_id = models.ForeignKey('Provider', on_delete=models.CASCADE)
-    service_id = models.ForeignKey('Service', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    provider_id = models.ForeignKey('Medicine.Provider', on_delete=models.CASCADE)
+    service_id = models.ForeignKey('Medicine.Service', on_delete=models.CASCADE)
     appointment_date = models.DateField()
     status = models.CharField(max_length=20, default='Pending')
     created_at = models.DateTimeField(auto_now_add=True)
