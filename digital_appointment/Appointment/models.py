fb33a54e9933148ffd30d8851004f35141998e09
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-08-05 00:08:45 +0330

    Set tables of appointment

--- digital_appointment/Appointment/models.py
+++ digital_appointment/Appointment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Appointment(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    provider_id = models.ForeignKey('Provider', on_delete=models.CASCADE)
+    service_id = models.ForeignKey('Service', on_delete=models.CASCADE)
+    appointment_date = models.DateField()
+    status = models.CharField(max_length=20, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
