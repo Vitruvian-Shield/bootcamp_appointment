cc345ab3a867d57fbd12613115ddd2448cbab712
ARASHSLG <yekadam17@gmail.com>
2024-08-04 23:34:29 +0330

    medicine file updated

---
+++ digital_appointment/medicine/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class LocationsModel(models.Model):
+    """ locations table """
+
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
