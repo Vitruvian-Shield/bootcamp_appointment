4bcb95529afbf1840acea00c70b713b841140c6a
AMSN81 <mramir886@gmail.com>
2024-08-05 00:57:54 +0330

    appointment,medicine apps and Provider,Location models created

--- digital_appointment/digital_appointment/settings.py
+++ digital_appointment/digital_appointment/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'appointment',
+    'medicine',
 ]
 
 MIDDLEWARE = [
