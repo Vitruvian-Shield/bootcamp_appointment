a44709314d55ed3e8f73b81907f288d7bf021583
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-08-05 00:18:17 +0330

    Set services, providers and locations in medicine app

--- digital_appointment/Medicine/models.py
+++ digital_appointment/Medicine/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    duration = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Provider(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    specialty = models.CharField(max_length=50)
+    location_id = models.ForeignKey('Locations', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.specialty
+
+
+class Locations(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField()
+    city = models.CharField(max_length=15)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
