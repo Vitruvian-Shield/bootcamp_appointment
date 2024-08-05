cc02a58ad0807c15a1718973480e1bbc50d1c5d0
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-08-05 16:49:12 +0330

    debug 2 lines

--- digital_appointment/Appointment/models.py
+++ digital_appointment/Appointment/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
+status_choices = (
+    ('s', 'scheduled'),
+    ('c', 'cancelled'),
+    ('b', 'completed')
+
+)
+
 
 class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     provider_id = models.ForeignKey('Medicine.Provider', on_delete=models.CASCADE)
     service_id = models.ForeignKey('Medicine.Service', on_delete=models.CASCADE)
-    appointment_date = models.DateField()
-    status = models.CharField(max_length=20, default='Pending')
+    appointment_date = models.DateTimeField()
+    status = models.CharField(max_length=1, choices=status_choices)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
