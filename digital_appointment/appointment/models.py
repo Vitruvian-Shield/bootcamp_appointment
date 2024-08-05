d9d74d00c3f20a62149830985424978464f72e3e
pooriya <adibradp@gmail.com>
2024-08-05 17:34:37 +0330

    the appointment class be set without assocition

--- digital_appointment/appointment/models.py
+++ digital_appointment/appointment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Appointment(models.Model):
+    """
+    this appointment model is used to store appointment details
+
+    """
+    class Status(models.TextChoices):
+        """
+        this enum is used to define the appointment status
+        """
+        sceduled = "Scheduled"
+        cancelled = "Cancelled"
+        completed = "Completed"
+
+    appointment_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=Status.choices, default=Status.sceduled, max_length=9)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
