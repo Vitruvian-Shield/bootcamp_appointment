bad7e36e0f1cced5a25a0034b1fe0ea8eea88908
younes <younes3021.us@gmail.com>
2024-08-05 23:00:28 +0330

    medicine app updated (added ProviderModel)

--- digital_appointment/medicine/models.py
+++ digital_appointment/medicine/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ..accounts.models import User, LocationsModel
 
 
 class ServicesModel(models.Model):
@@ -6,9 +7,17 @@ class ServicesModel(models.Model):
 
     name = models.CharField(max_length=50)
     description = models.TextField()
-    duration = models.DurationField()
-    price = models.DecimalField()
+    duration = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class ProviderModel(models.Model):
+    """Provider Table"""
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_id")
+    speciality = models.CharField(max_length=100)
+    location_id = models.ForeignKey(LocationsModel, on_delete=models.CASCADE, related_name="location_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
