4471b8419a51fb1c5978a1e9444a07004f2fd5da
pooriya <adibradp@gmail.com>
2024-08-05 17:15:48 +0330

    medicine app is created

--- digital_appointment/digital_appointment/settings.py
+++ digital_appointment/digital_appointment/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    "appointment.apps.AppointmentConfig"
+    "appointment.apps.AppointmentConfig",
+    "medicine.apps.MedicineConfig"
 ]
 
 MIDDLEWARE = [
