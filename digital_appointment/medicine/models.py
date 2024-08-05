eae03e5770251c33093fb275a6ed01b51d9c6f2a
pooriya <adibradp@gmail.com>
2024-08-05 17:56:50 +0330

    Locations model be set like previous model without assocition

--- digital_appointment/medicine/models.py
+++ digital_appointment/medicine/models.py
@@ -17,3 +17,13 @@ class Provider(models.Model):
     specialty = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
