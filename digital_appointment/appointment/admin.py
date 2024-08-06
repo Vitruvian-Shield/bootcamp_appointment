e454d331442812dd45d2014d2361ff8e2fd71bcb
pooriya <adibradp@gmail.com>
2024-08-06 18:07:34 +0330

    the appointment admin be set in admin panel

--- digital_appointment/appointment/admin.py
+++ digital_appointment/appointment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    """
+    this for displaying appointments details and there attribute in DataBase
+    """
+    list_display = ('id', 'appointment_date', 'status', 'created_date', 'updated_date')
