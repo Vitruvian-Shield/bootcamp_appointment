a862609a916018f54090415c762529734a026040
erfan <erfannaderi081@gmail.com>
2024-08-06 17:30:38 +0330

    create appointment app

--- digital_appointment/digital_appointment/settings.py
+++ digital_appointment/digital_appointment/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'appointment.apps.AppointmentConfig',
 ]
 
 MIDDLEWARE = [
