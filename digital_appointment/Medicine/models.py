55b6606195354e2d762a891540b2ce7047e27a2a
RezaNajjarF <reza.najjar.79@gmail.com>
2024-08-07 13:43:26 +0330

    Providers model added

--- digital_appointment/Medicine/models.py
+++ digital_appointment/Medicine/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from digital_appointment.accounts.models import User
+
 
 class Locations(models.Model):
     name = models.CharField(max_length=100)
@@ -8,7 +10,7 @@ class Locations(models.Model):
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    updates_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -20,7 +22,15 @@ class Services(models.Model):
     duration = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
-    updates_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class Providers(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    speciality = models.CharField(max_length=200)
+    locations_id = models.ForeignKey(Locations, on_delete=models.DO_NOTHING)  # khode location behtar nist mohem nist kheali
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
