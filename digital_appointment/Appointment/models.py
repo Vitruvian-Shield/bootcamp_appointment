cb86db5f886a4f2d900285a6587f39a68af6cfc0
Soroush-izp <soroushizadpanah@yahoo.com>
2024-08-07 19:55:35 +0330

    add Providers Table in Medicine models file

--- digital_appointment/Appointment/models.py
+++ digital_appointment/Appointment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User
+from Medicine.models import Services
 
 class Appointments(models.Model):
    STATUS_CHOICES  = {'SC': 'Scheduled', 'CO': 'Completed', 'CA': 'Canceled'}
@@ -7,7 +8,7 @@ class Appointments(models.Model):
    id = models.AutoField(primary_key=True)
    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
    # provider_id = models.ForeignKey(Providers, on_delete=models.CASCADE)
-   # service_id = models.ForeignKey(Services, on_delete=models.CASCADE)
+   service_id = models.ForeignKey(Services, on_delete=models.CASCADE)
    appointment_date = models.DateTimeField()
    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='SC')
    created_at = models.DateTimeField(auto_now_add=True)    # first time added
--- digital_appointment/Medicine/models.py
+++ digital_appointment/Medicine/models.py
@@ -15,3 +15,15 @@ class Services(models.Model):
       return f"Name: {self.name}, Description: {self.description}"
 
 
+class Providers(models.Model):
+   id = models.AutoField(primary_key=True)
+   user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+   specialty = models.CharField()
+   # location_id = models.ForeignKey(Locations, on_delete=models.CASCADE)
+   created_at = models.DateTimeFiled(auto_now_add=True)
+   updated_at = models.DateTimeFiled(auto_now=True)
+   
+   def __str__(self):
+      return f"UserId: {self.user_id}, Specialty: {self.specialty}"
+
+
