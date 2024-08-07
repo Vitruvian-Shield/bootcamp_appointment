5bfb68939578fd0d76ea8f0a2b4cdda63f64371e
Soroush-izp <soroushizadpanah@yahoo.com>
2024-08-07 20:21:13 +0330

    Edite Appointments Table

--- digital_appointment/Appointment/models.py
+++ digital_appointment/Appointment/models.py
@@ -3,7 +3,11 @@ from accounts.models import User
 from Medicine.models import Services, Providers
 
 class Appointments(models.Model):
-   STATUS_CHOICES  = {'SC': 'Scheduled', 'CO': 'Completed', 'CA': 'Canceled'}
+   STATUS_CHOICES = [
+      ('SCH', 'Scheduled'),
+      ('COM', 'Completed'),
+      ('CAN', 'Canceled'),
+]
    
    id = models.AutoField(primary_key=True)
    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,4 +19,4 @@ class Appointments(models.Model):
    updated_at = models.DateTimeField(auto_now=True)    # last update
    
    def __str__(self):
-      return f"Id: {self.id}, UserId: {self.user_id}, Status: {self.status}"
+      return f"UserId: {self.user_id}, ProviderId: {self.provider_id}, AppointmentDate: {self.appointment_date}"
