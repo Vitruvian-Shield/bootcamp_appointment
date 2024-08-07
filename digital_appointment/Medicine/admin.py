20d2bb54675c20658b487fda21ef168e490e33c0
Soroush-izp <soroushizadpanah@yahoo.com>
2024-08-07 20:34:20 +0330

    register Medicine Tables for admin

--- digital_appointment/Medicine/admin.py
+++ digital_appointment/Medicine/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Locations, Services, Providers
 
-# Register your models here.
+admin.site.register( Locations)
+admin.site.register( Services)
+admin.site.register( Providers)
