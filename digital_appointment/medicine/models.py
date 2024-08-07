8f7492f44815d8025178e3284cdc7c31c515d603
nima <ssu87556@gmail.com>
2024-08-07 17:20:56 +0330

    add Service model

--- digital_appointment/medicine/models.py
+++ digital_appointment/medicine/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class Service(BaseModel):
+     name = models.CharField(max_length=128)
+     description = models.TextField()
+     duration = models.DurationField()
+     price = models.DecimalField(max_digits=12, decimal_places=2)
+
