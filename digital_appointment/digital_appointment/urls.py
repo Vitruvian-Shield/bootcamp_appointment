85b4f7caeea860b06058ff4894b12ef8e53b6a86
Ali <r.razifar1999@gmail.com>
2024-08-10 09:31:10 +0330

    urls are updated

--- digital_appointment/digital_appointment/urls.py
+++ digital_appointment/digital_appointment/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('appointment/', include('appointment.urls')),
+    path('medicine/', include('medicine.urls'))
 ]
