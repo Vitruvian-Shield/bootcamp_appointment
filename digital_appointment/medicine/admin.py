8b8f0026e0ce37445d6dafc53a7b7bd942f70aaa
Ali <r.razifar1999@gmail.com>
2024-08-11 10:07:07 +0330

    models are updated

--- digital_appointment/medicine/admin.py
+++ digital_appointment/medicine/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Service)
+admin.site.register(models.Provider)
+admin.site.register(models.Location)
