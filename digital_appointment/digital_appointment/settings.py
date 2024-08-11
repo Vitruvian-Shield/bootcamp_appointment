12875c5a0b8f0b8b0b1907d784b0bd20b136ae16
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-08-11 23:24:01 +0330

    change database to postgresql

--- digital_appointment/digital_appointment/settings.py
+++ digital_appointment/digital_appointment/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # APP
     'accounts.apps.AccountsConfig',
     'medicine.apps.MedicineConfig',
     'appointment.apps.AppointmentConfig',
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'digital_appointment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appointment',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
