de0a186791a8dd695a11439ec98594ca60d680c4
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-08-11 10:09:52 +0330

    Add models to admin.py

--- digital_appointment/Medicine/admin.py
+++ digital_appointment/Medicine/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Provider)
+admin.site.register(models.Locations)
+admin.site.register(models.Service)
