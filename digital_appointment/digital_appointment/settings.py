315fa6a0908dd207cf21ff8538a9282e0f43ce8a
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-08-11 10:10:16 +0330

    change DB engine

--- digital_appointment/digital_appointment/settings.py
+++ digital_appointment/digital_appointment/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'digital_appointment.wsgi.application'
 
 DATABASES = {
    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
        'NAME': 'verna',
        'USER': 'postgres',
        'PASSWORD': 'verna',
