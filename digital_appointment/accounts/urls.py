dd55aa8e1620624e912d715bcd3865ed8581ab84
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-08-12 11:32:51 +0330

    set namespace to accounts urls in digital_appointment and name to urls in apps

--- digital_appointment/accounts/urls.py
+++ digital_appointment/accounts/urls.py
@@ -1,6 +1,14 @@
 from . import views
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
+app_name = 'accounts'
 urlpatterns = [
-    path("users/", views.get_users)
+    path("user/", views.UserList.as_view(), name="user_list"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path("user/<int:pk>/", views.UserDetail.as_view(), name='user_detail'),
+    path('profile/', views.ProfileView.as_view(), name='user_profile'),
 ]
