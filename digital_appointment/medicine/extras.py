0df7d3ef503eb3b13e9dd2d129f4d04053a2ac37
RezaNajjarF <reza.najjar.79@gmail.com>
2024-08-25 20:46:57 +0330

    models updated: choices and docstring added

---
+++ digital_appointment/medicine/extras.py
@@ -0,0 +1,22 @@
+# all extra things like choices would be here
+
+from abc import ABC
+
+
+class Choices:
+    """all choices would be accessible by this class"""
+
+    @staticmethod
+    def provider_speciality():
+        """list of providers specialties.
+
+        Returns:
+            dict: provider speciality choices
+
+        """
+
+        return {'FM': 'Family medicine',
+                'IM': 'Internal medicine',
+                'EM': 'Emergency medicine',
+                'OT': 'Other medicine'
+                }
--- digital_appointment/medicine/serializers.py
+++ digital_appointment/medicine/serializers.py
@@ -6,3 +6,8 @@ class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Location
         fields = '__all__'
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Location
+        fields = '__all__'
