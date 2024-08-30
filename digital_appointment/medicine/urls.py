681b3005b11626f4d6c7177e440d7c86545124ad
pooriya <adibradp@gmail.com>
2024-08-30 18:20:48 +0330

    added loactions path to url.py

--- digital_appointment/medicine/urls.py
+++ digital_appointment/medicine/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Provider, SpecialtyListView
+from .views import Provider, SpecialtyListView, Location
 
 urlpatterns = [
     path('provider/', Provider.as_view()),
-    path('speciality/list/', SpecialtyListView.as_view())
+    path('speciality/list/', SpecialtyListView.as_view()),
+    path('locations/', Location.as_view())
 ]
