898d37029e377c56135aa0e590e8b948bb256f93
pooriya <adibradp@gmail.com>
2024-08-30 19:31:52 +0330

    Interactions views.py Comment be updated

--- digital_appointment/interaction/views.py
+++ digital_appointment/interaction/views.py
@@ -1,10 +1,14 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt import authentication
 from . import serializers, models
 
 
 class Comment(APIView):
+    authentication_classes = [authentication.JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         serializer = serializers.CommentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
