45a598e81e3d9da735970ca022d5a97a220dceed
pooriya <adibradp@gmail.com>
2024-08-30 19:42:38 +0330

    medicine admin.py be updated

--- digital_appointment/medicine/admin.py
+++ digital_appointment/medicine/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from . import models
+from interaction.models import Comment
 
+
+@admin.register(models.Provider)
+class ProviderAdmin(admin.ModelAdmin):
+
+    list_display = ('user','location', 'speciality')
 
 admin.site.register(models.Service)
-admin.site.register(models.Provider)
 admin.site.register(models.Location)
