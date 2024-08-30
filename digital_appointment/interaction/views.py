d43de598db17805dd074325909140361b9c32e72
pooriya <adibradp@gmail.com>
2024-08-30 19:28:08 +0330

    Interactions views.py be set with Post Comment api

--- digital_appointment/interaction/views.py
+++ digital_appointment/interaction/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from . import serializers, models
 
-# Create your views here.
+
+class Comment(APIView):
+    def post(self, request):
+        serializer = serializers.CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
