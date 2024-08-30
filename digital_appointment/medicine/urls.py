0169f9e5064ef4da8c7f6ba9916fb65becf6289e
pooriya <adibradp@gmail.com>
2024-08-30 19:15:12 +0330

    ProviderDetails Api urls be set

--- digital_appointment/medicine/urls.py
+++ digital_appointment/medicine/urls.py
@@ -4,5 +4,6 @@ from .views import Provider, SpecialtyListView, LocationApi
 urlpatterns = [
     path('provider/', Provider.as_view()),
     path('speciality/list/', SpecialtyListView.as_view()),
-    path('locations/', LocationApi.as_view())
+    path('locations/', LocationApi.as_view()),
+    path('ProviderDetail/<int:pk>/', Provider.as_view()),
 ]
