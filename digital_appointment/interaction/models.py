64dec3886e816c281b78750be7e701a220441ef3
pooriya <adibradp@gmail.com>
2024-08-30 18:50:25 +0330

    intraction Comment model be set

--- digital_appointment/interaction/models.py
+++ digital_appointment/interaction/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
-# Create your models here.
+from accounts.models import User
+from medicine.models import Provider
+class Comment(models.Model):
+    """
+    this model be used to store comments
+    """
+    """relational"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    """attributes"""
+    comment = models.TextField()
+    def __str__(self):
+        return self.comment
