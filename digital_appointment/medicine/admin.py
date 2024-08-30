76ca0651a9b7da375a18db76335cf33358e6cd17
pooriya <adibradp@gmail.com>
2024-08-30 19:43:25 +0330

    Comment be added to medicine admin.py provider subset

--- digital_appointment/medicine/admin.py
+++ digital_appointment/medicine/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from . import models
 from interaction.models import Comment
 
-
+class CommentAdmin(admin.TabularInline):
+    model = Comment
+    extra = 1
 @admin.register(models.Provider)
 class ProviderAdmin(admin.ModelAdmin):
-
+    inlines = [CommentAdmin]
     list_display = ('user','location', 'speciality')
 
 admin.site.register(models.Service)
