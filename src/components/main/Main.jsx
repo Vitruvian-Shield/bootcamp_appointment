c0fd7c87aea88b957d3113d5606d5aafd5a1aeb5
ARASHSLG <yekadam17@gmail.com>
2024-08-31 23:07:26 +0330

    main file updated

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,30 +3,31 @@ import main from './main.css'
 import { FaSearch } from "react-icons/fa"
 import { useNavigate } from "react-router-dom"
 import SearchPage from "../searchpage/SpecialitySearch"
-import {Box, Grid} from '@mui/material'
+import axios from "axios"
+
+axios.get()
 
 
-class Main extends React.Component{
-    state = {
-        open : false,
-        setOpen: false
-    }
+const Main = () => {
+   
 
     // onChange function
-    openrHandler = event => !this.state.setOpen 
-    navigate = event => useNavigate(SearchPage)
+    const navigate = useNavigate()
 
-    render(){
         return(
             <div style={main}className="main" id="main">
+                
+                {/* /////// header //////// */}
                 <div className="main">
                     <h1>به سامانه نوبت دهی آنلاین ویترووین شیلد خوش آمدید</h1>
                     <p className="main"> با استفاده از سیستم نوبت دهی انلاین ما ، به راحتی میتوانید پزشکان مورد نظر خود را بیابید و نوبت بگیرید.</p>
                 </div>
+
+                {/* //////// search section //////// */}
                 <div className="main" id='search'>
                     <h2> فقط کافیست نام پزشک یا تخصص مورد نظر خود را در باکس جستجو وارد کنید<br></br> و از خدمات باکیفیت ما بهره‌مند شوید. </h2>
                     <span id="searchspan">
-                        <button className="main" id="searchbutton" name="searchbutton" /*onClick=/*{this.navigate}*/><b>جستجو</b>
+                        <button className="main" id="searchbutton" name="searchbutton" onClick= {navigate('/search')}><b>جستجو</b>
                         <FaSearch id="searchicon" />
                         </button>
                         <input name="searchbar" id="searchbar" placeholder=" نام پزشک ، تخصص ، بیماری و شهر و . . ." type="text"></input>
@@ -35,6 +36,6 @@ class Main extends React.Component{
             </div>
         )
     }
-}
+
 
 export default Main
