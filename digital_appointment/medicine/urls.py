46901c7ff0fd1a31c64c5f487e0b8c46882887d5
pooriya <adibradp@gmail.com>
2024-09-02 21:25:04 +0330

    medicine.url be updated

--- digital_appointment/medicine/urls.py
+++ digital_appointment/medicine/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import Provider, SpecialtyListView, LocationApi, ProviderDetail
+from .views import Provider, SpecialtyListView, Location, ProviderDetail
 
 urlpatterns = [
     path('provider/', Provider.as_view()),
     path('speciality/list/', SpecialtyListView.as_view()),
-    path('locations/', LocationApi.as_view()),
+    path('locations/', Location.as_view()),
     path('ProviderDetail/<int:pk>/', ProviderDetail.as_view()),
 ]
