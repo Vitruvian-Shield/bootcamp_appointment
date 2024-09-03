639562eee938f059a4dc9d0a28c5096cdd85467e
pooriya <adibradp@gmail.com>
2024-09-03 20:58:07 +0330

    set django caches in setting config

--- digital_appointment/digital_appointment/settings.py
+++ digital_appointment/digital_appointment/settings.py
@@ -100,6 +100,16 @@ DATABASES = {
     },
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+        'KEY_PREFIX': 'django_redis_cache',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
