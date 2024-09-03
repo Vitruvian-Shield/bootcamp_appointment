e69f40db1216296a3ec1eec81b6a1e216509ba08
pooriya <adibradp@gmail.com>
2024-09-04 01:56:55 +0330

    account url be updated

--- digital_appointment/accounts/urls.py
+++ digital_appointment/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("user/", views.User.as_view()),
     path("user/<int:pk>/", views.UserDetail.as_view()),
     path("profile/", views.ProfileView.as_view()),
-    path('auth/google/callback/', views.google_callback, name='google_callback'),
+    path('auth/google/callback/', views.GoogleCallback.as_view(), name='google_callback'),
     path('sms/', views.SmsAuthentication.as_view(), name='sms'),
     path('sms/verify', views.VerifyCodeSmsAuthentication.as_view(), name='sms'),
 ]
