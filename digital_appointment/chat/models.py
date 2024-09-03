b74a1afbf5f43b00133f7149a0f7056decb71d70
nima <ssu87556@gmail.com>
2024-09-03 16:20:45 +0330

    add Rate to chat.models

--- digital_appointment/chat/models.py
+++ digital_appointment/chat/models.py
@@ -6,10 +6,25 @@ class Comment(models.Model):
     comment = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     update = models.BooleanField(default=False)
-
     class Meta:
         ordering = ['date']
-
-
+        
     def __str__(self):
         return 'Comment {} by {}'.format(self.comment, self.user)
+    
+class Rate(models.Model):
+    provider = models.ForeignKey("medicine.Provider", on_delete=models.CASCADE, related_name="rates", null=True)
+    comment = models.ForeignKey("chat.Comment", on_delete=models.CASCADE, related_name="commet_rates", null=True)
+    score = models.PositiveSmallIntegerField(choices=[(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")])
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "rate"
+        constraint = [
+            models.UniqueConstraint(fields=["provider", "user"], name="unique_rate"),
+            models.UniqueConstraint(fields=["comment", "user"], name="unique_comment_rate")
+        ]
+
+    
