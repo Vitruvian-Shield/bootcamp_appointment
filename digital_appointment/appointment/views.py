1fde072f3cfdd9ee65d05cfd977c6c1cb0e8637d
verna <amirvernaw@gmail.com>
2024-09-03 15:00:21 +0330

    Added REST API views for appointments with GET functionality

--- digital_appointment/appointment/views.py
+++ digital_appointment/appointment/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from . import models
+from . import serializers
+from rest_framework import views, status
+from rest_framework.response import Response
+from rest_framework import permissions, pagination
+from rest_framework_simplejwt import authentication
 
-# Create your views here.
+
+class Appointment(views.APIView, pagination.PageNumberPagination):
+    authentication_classes = [authentication.JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+
+            appointments = models.Appointment.objects.filter(user=request.user).order_by("-date")
+            page = self.paginate_queryset(appointments, request)
+            serializer = serializers.AppointmentSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
