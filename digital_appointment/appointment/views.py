3657d271719e845fefbdc8fa23a7ab76bce08802
pooriya <adibradp@gmail.com>
2024-09-04 21:17:28 +0330

    appointment api be creared

--- digital_appointment/appointment/views.py
+++ digital_appointment/appointment/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework_simplejwt import authentication
+from . import serializers
+from .models import Appointment
+from django.db.models import Q
 
-# Create your views here.
+class AppointmentView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.JWTAuthentication]
+
+    def post(self, request):
+        serializer = serializers.AppointmentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        validate_appointment = Appointment.objects.filter(user=request.user)
+        serializer = serializers.AppointmentSerializer(validate_appointment, many=True)
+        return Response(serializer.data)
+
