2639d2ae4a22511dc215555c45011830c0609b5d
pooriya <adibradp@gmail.com>
2024-09-05 03:26:32 +0330

    setting pagination config be changed

--- digital_appointment/digital_appointment/settings.py
+++ digital_appointment/digital_appointment/settings.py
@@ -132,8 +132,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 SPECTACULAR_SETTINGS = {
