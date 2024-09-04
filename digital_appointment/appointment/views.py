67ede6a1a1c38d113ba95035c78d3b34daf4daa5
pooriya <adibradp@gmail.com>
2024-09-04 21:22:36 +0330

    added guide comment to api

--- digital_appointment/appointment/views.py
+++ digital_appointment/appointment/views.py
@@ -5,8 +5,14 @@ from rest_framework_simplejwt import authentication
 from . import serializers
 from .models import Appointment
 from django.db.models import Q
+from drf_spectacular.utils import extend_schema
+
 
 class AppointmentView(APIView):
+    """
+    permission to any authenticate user use post or personal get
+    if user is provider or patient work for both
+    """
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.JWTAuthentication]
 
@@ -21,22 +27,30 @@ class AppointmentView(APIView):
         serializer = serializers.AppointmentSerializer(validate_appointment, many=True)
         return Response(serializer.data)
 
+
 class AppointmentDetailView(APIView):
+    """
+    permission to any authenticate user use retrieve or put or delete
+    method if user is provider or patient work for both
+    """
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.JWTAuthentication]
+
     def get(self, request, pk):
         validate_appointment = Appointment.objects.filter(
-            Q(user=request.user)|
+            Q(user=request.user) |
             Q(id=pk)
         ).first()
         serializer = serializers.AppointmentSerializer(validate_appointment)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     def put(self, request, pk):
         validate_appointment = Appointment.objects.filter(user=request.user, id=pk).first()
         serializer = serializers.AppointmentSerializer(validate_appointment, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     def delete(self, request, pk):
         validate_appointment = Appointment.objects.filter(user=request.user, id=pk).first()
         if validate_appointment:
