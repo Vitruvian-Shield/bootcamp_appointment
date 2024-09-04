e4c30677dab0aa07442b9d8c0c0b893dd7740eb7
pooriya <adibradp@gmail.com>
2024-09-04 21:17:51 +0330

    appointment details api be created

--- digital_appointment/appointment/views.py
+++ digital_appointment/appointment/views.py
@@ -21,3 +21,24 @@ class AppointmentView(APIView):
         serializer = serializers.AppointmentSerializer(validate_appointment, many=True)
         return Response(serializer.data)
 
+class AppointmentDetailView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.JWTAuthentication]
+    def get(self, request, pk):
+        validate_appointment = Appointment.objects.filter(
+            Q(user=request.user)|
+            Q(id=pk)
+        ).first()
+        serializer = serializers.AppointmentSerializer(validate_appointment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def put(self, request, pk):
+        validate_appointment = Appointment.objects.filter(user=request.user, id=pk).first()
+        serializer = serializers.AppointmentSerializer(validate_appointment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def delete(self, request, pk):
+        validate_appointment = Appointment.objects.filter(user=request.user, id=pk).first()
+        if validate_appointment:
+            validate_appointment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
