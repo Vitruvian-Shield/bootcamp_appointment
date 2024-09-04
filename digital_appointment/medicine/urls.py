e52faacb1aa90f8ee3060f48f83dd8fbf036f6d5
pooriya <adibradp@gmail.com>
2024-09-04 20:42:59 +0330

    added Service url

--- digital_appointment/medicine/urls.py
+++ digital_appointment/medicine/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Provider, SpecialtyListView, Location, ProviderDetail
+from .views import Provider, SpecialtyListView, Location, ProviderDetail,Service
 
 urlpatterns = [
     path('provider/', Provider.as_view()),
     path('speciality/list/', SpecialtyListView.as_view()),
     path('locations/', Location.as_view()),
     path('ProviderDetail/<int:pk>/', ProviderDetail.as_view()),
+    path('Service/', Service.as_view()),
 ]
