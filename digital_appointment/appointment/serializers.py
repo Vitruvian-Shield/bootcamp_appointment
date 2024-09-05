cce13445aa8cb44f33211339d8acb5ddfc3ec45e
younes <younes3021.us@gmail.com>
2024-09-05 20:45:17 +0330

    appointment serializer updated

--- digital_appointment/appointment/serializers.py
+++ digital_appointment/appointment/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from accounts import serializers as accounts_serializers
+from accounts.models import User
+from medicine.models import DoctorsModel
 from medicine import serializers as medicine_serializers
 from . import models
 
@@ -19,8 +21,8 @@ class AppointmentSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     """serializer for comments in profile page"""
 
-    user = accounts_serializers.UserSerializer()
-    doctor = medicine_serializers.DoctorsSerializer()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    doctor = serializers.PrimaryKeyRelatedField(queryset=DoctorsModel.objects.all())
 
     class Meta:
         model = models.CommentsModel
