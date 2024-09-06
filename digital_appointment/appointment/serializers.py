320ead987092dcf7bd008e7f7bf4ec8a32da8edb
pooriya <adibradp@gmail.com>
2024-09-06 06:21:32 +0330

    appointment serilizer issue be fixed

--- digital_appointment/appointment/serializers.py
+++ digital_appointment/appointment/serializers.py
@@ -1,26 +1,29 @@
 from rest_framework import serializers
 from appointment.models import Appointment
-"""
-read comment in top of appotinment serializer you get
-reason of this comment 
-
 from accounts import serializers as account_serializers
 from medicine import serializers as medicine_serializers
-"""
-class AppointmentSerializer(serializers.ModelSerializer):
-    """
-    i write this because be like medicine serializer
-    but access level be important for me and
-    when patient want get appointment access level
-    dont permission to us create user provider, service and etc.
-    this code only write for competition in this bootcamp
-    and this not practical in this policy and say we can
-    write a serializer that have power created many model
+from accounts.models import User
+from medicine.models import *
 
-    user = account_serializers.UserSerializer()
-    provider = medicine_serializers.ProviderSerializer()
-    service = medicine_serializers.ServiceSerializer()
-    """
+class AppointmentSerializer(serializers.ModelSerializer):
+    user = account_serializers.UserSerializer(read_only=True)
+    provider = medicine_serializers.ProviderSerializer(read_only=True)
+    service = medicine_serializers.ServiceSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        source='user',
+        write_only=True,
+    )
+    provider_id = serializers.PrimaryKeyRelatedField(
+        queryset=Provider.objects.all(),
+        source='provider',
+        write_only=True,
+    )
+    service_id = serializers.PrimaryKeyRelatedField(
+        queryset=Service.objects.all(),
+        source='service',
+        write_only=True,
+    )
 
     class Meta:
         model = Appointment
