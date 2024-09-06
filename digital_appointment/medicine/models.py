e9995f9d83b689855e29e37aa732694e910a54fb
Amirhossein Ghaffari <amirvernaw@gmail.com>
2024-09-06 17:18:08 +0330

    rating system and signal sets

--- digital_appointment/medicine/models.py
+++ digital_appointment/medicine/models.py
@@ -5,6 +5,7 @@ class Provider(models.Model):
     user = models.OneToOneField('accounts.User', on_delete=models.CASCADE)
     speciality = models.CharField(max_length=255)
     location = models.ForeignKey('medicine.Location', on_delete=models.CASCADE)
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
@@ -44,3 +45,16 @@ class Service(models.Model):
 
     class Meta:
         db_table = "service"
+
+class Rating(models.Model):
+    provider = models.ForeignKey('Provider', on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    review = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Rating for {self.provider} - {self.rating}"
+
+    class Meta:
+        db_table = "rating"
