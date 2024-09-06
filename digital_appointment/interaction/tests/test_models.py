b1b59eac63ce8a09919ef79cbb0de6b7de77af02
pooriya <adibradp@gmail.com>
2024-09-06 19:40:55 +0330

    added comment to test_models for guide

---
+++ digital_appointment/interaction/tests/test_models.py
@@ -0,0 +1,12 @@
+"""
+in this file we test method of class that created
+in model.py and we for test need many thing:
+1.setup test:
+    we must create a object from this class
+    (instance of class) with set value in
+    attribute and we must test output of
+    this method that we havent in this app
+    so we havent model test in this app
+    but i created this file for guide
+    we have test model in account application
+"""
