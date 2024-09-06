12d1cd2ed2085c59c51f616b86277b23eda854f9
pooriya <adibradp@gmail.com>
2024-09-06 22:46:21 +0330

    account url be updated

--- digital_appointment/accounts/urls.py
+++ digital_appointment/accounts/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("login/", views.CustomTokenObtainPairView.as_view()),
-    path("login/refresh/", TokenRefreshView.as_view()),
-    path("user/", views.User.as_view()),
-    path("user/<int:pk>/", views.UserDetail.as_view()),
-    path("profile/", views.ProfileView.as_view()),
-    path('auth/google/start/', views.GoogleAuthInit.as_view(), name='google_callback'),
+    path("login/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("user/", views.User.as_view(), name="user"),
+    path("user/<int:pk>/", views.UserDetail.as_view(), name="user_detail"),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path('auth/google/start/', views.GoogleAuthInit.as_view(), name='google_auth'),
     path('auth/google/callback/', views.GoogleCallback.as_view(), name='google_callback'),
     path('auth/sms/', views.SmsAuthentication.as_view(), name='sms'),
-    path('sms/verify', views.VerifyCodeSmsAuthentication.as_view(), name='sms'),
+    path('auth/sms/verify/', views.VerifyCodeSmsAuthentication.as_view(), name='sms-verify'),
 ]
